90a2587fd50107d991eb89b94827af81b5a6ca4c
Rüveyda Dal <71987230+ruv19@users.noreply.github.com>
2021-04-26 17:15:24 +0300

    Add files via upload

---
+++ pyhton ile tahmin oyunu.py
@@ -0,0 +1,21 @@
+from random import randint
+rand=randint(1,100)
+sayac=0
+
+print('SAYİ TAHMİN OYUNUNA HOSGELDİNİZ!')
+
+
+while True:
+    sayac+=1
+    sayi1=int(input('1 ile 100 arasindan bir sayi tahmin ediniz'))
+    if (sayi1==rand):
+        print('sayiyi bildiniz:)')
+        print('secilen sayi= {}'.format(sayi1))
+        break
+    elif sayi1>rand:
+        print('girdiginiz sayi secilen sayidan buyuktur!')
+        continue
+    elif sayi1<rand:
+        print('girdiginiz sayi secilen sayidan kucuktur!') 
+        continue
+  
